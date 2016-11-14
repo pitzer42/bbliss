1f65f908bcd463d5528ff1f58d9cf816113528b9
Arthur Pitzer <arthurpitzer@id.uff.br>
2016-11-14 09:52:02 -0200

    quick deploy

--- src/iceServers.js
+++ src/iceServers.js
@@ -1,14 +1,16 @@
 module.exports = {
   'iceServers': [{
     'urls': [
-      'stun:stun.l.google.com:19302',
-      'stun:stun01.sipphone.com',
-      'stun:stun.ekiga.net',
-      'stun:stunserver.org',
-      'stun:stun4.l.google.com:19302',
-      'stun:stun3.l.google.com:19302',
-      'stun:stun2.l.google.com:19302',
-      'stun:stun1.l.google.com:19302'
+      'stun:stun.l.google.com:19302'
     ]
   }]
 }
+/*
+'stun:stun01.sipphone.com',
+'stun:stun.ekiga.net',
+'stun:stunserver.org',
+'stun:stun4.l.google.com:19302',
+'stun:stun3.l.google.com:19302',
+'stun:stun2.l.google.com:19302',
+'stun:stun1.l.google.com:19302'
+*/
